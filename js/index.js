104d945edc3ebec39992bdbb6da9fa1c1dfe9bab
cuttarug <lpolepeddi@gmail.com>
2013-05-18 00:25:43 -0600

    Snap (hardcoded)

--- js/index.js
+++ js/index.js
@@ -4,6 +4,8 @@ var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
 var touchStartX = 0;
 var navStart = 0;
 var mainStart = 0;
+var navCurrent = 0;
+var mainCurrent = 0;
 
 main.addEventListener('touchstart', function(event) {
   touchStartX = event.touches[0].pageX;
@@ -21,6 +23,19 @@ main.addEventListener('touchmove', function(event) {
   var touch = event.targetTouches[0];
   var dragX = (touch.pageX - touchStartX);
   var percent = (dragX / width) * 100;
-  nav.style.left = navStart + percent + '%';
-  main.style.left = mainStart + percent + '%';
+  navCurrent = navStart + percent;
+  nav.style.left = navCurrent + '%';
+  mainCurrent = mainStart + percent;
+  main.style.left = mainCurrent + '%';
+}, false);
+
+main.addEventListener('touchend', function(event) {
+  if(mainCurrent < 40) {
+    nav.style.left = "-100%";
+    main.style.left = "0%";
+  }
+  else {
+    nav.style.left = "0%";
+    main.style.left = "100%";    
+  }
 }, false);

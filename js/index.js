993d6c9de69b3db4948e08c1fafe06a0b16ceeef
cuttarug <lpolepeddi@gmail.com>
2013-06-04 20:28:58 -0600

    Event bubbling

---
+++ js/index.js
@@ -0,0 +1,17 @@
+$('#main-container').click(function(event) {
+  if(!$(this).hasClass('active') && event.target.id == 'main-container') {
+    $('#nav-container').transition({x:'-100%'}, 200, 'ease');
+    $('#main-container').transition({x:'0%'}, 200, 'ease');
+    $('#main-container').addClass('active');
+    $('#nav-container').removeClass('active');
+  }
+});
+
+$('#menu').click(function(event) {
+  if($('#main-container').hasClass('active')) {
+    $('#nav-container').transition({x:'0%'}, 200, 'ease');
+    $('#main-container').transition({x:'65%'}, 200, 'ease');
+    $('#nav-container').addClass('active');
+    $('#main-container').removeClass('active');
+  }
+});
